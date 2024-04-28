d3830063dbac353166c111abcde6297c6d775227
ddddddO <lbfdeatq@gmail.com>
2024-04-28 11:06:57 +0900

    up

--- ipv4.go
+++ ipv4.go
@@ -2,18 +2,18 @@ package main
 
 // https://www.infraexpert.com/study/tcpip1.html
 type ipv4 struct {
-	version        uint8
-	ihl            uint8  // hearder length
-	tos            uint8  // type of service
-	totalLength    uint16 // total length
-	identification uint16
-	flags          uint8
-	fragmentOffset uint16
-	ttl            uint8
-	protocol       uint8
-	headerChecksum uint16
-	srcAddr        uint32
-	dstAddr        uint32
+	version        uint8  // 4bit
+	ihl            uint8  // 4bit. hearder length
+	tos            uint8  // 8bit. type of service
+	totalLength    uint16 // 16bit. total length
+	identification uint16 // 16bit
+	flags          uint8  // 3bit
+	fragmentOffset uint16 // 13bit
+	ttl            uint8  // 8bit
+	protocol       uint8  // 8bit
+	headerChecksum uint16 // 16bit
+	srcAddr        uint32 // 32bit
+	dstAddr        uint32 // 32bit
 
 	options []uint8
 	padding []uint8

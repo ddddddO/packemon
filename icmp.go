4e1f53a7bb3d965cbdb7c7b9ddd884338adc0e8d
ddddddO <lbfdeatq@gmail.com>
2024-04-30 23:32:32 +0900

    icmp checksum

--- icmp.go
+++ icmp.go
@@ -6,33 +6,37 @@ import (
 )
 
 // https://www.infraexpert.com/study/tcpip4.html
+// https://inc0x0.com/icmp-ip-packets-ping-manually-create-and-send-icmp-ip-packets/
 type icmp struct {
-	typ      uint8
-	code     uint8
-	checksum uint16
-	data     []byte
+	typ        uint8
+	code       uint8
+	checksum   uint16
+	identifier uint16
+	sequence   uint16
+	data       []byte
 }
 
 const (
 	ICMP_TYPE_REQUEST = 0x08
 )
 
-// https://inc0x0.com/icmp-ip-packets-ping-manually-create-and-send-icmp-ip-packets/
+// icmp request
 func newICMP() *icmp {
-	var (
-	// identifier1 uint16 = 0x1234
-	// identifier2 uint16 = 0x9876
-	// sequence1 uint16 = 0x0001
-	// sequence2 uint16 = 0x0100
-	// data = make([]byte, 4)
-	)
-
-	return &icmp{
-		typ:      ICMP_TYPE_REQUEST,
-		code:     0,
-		checksum: 0xe5ca,
-		data:     []byte{0x12, 0x34, 0x98, 0x76, 0x00, 0x01, 0x01, 0x00},
+	icmp := &icmp{
+		typ:        ICMP_TYPE_REQUEST,
+		code:       0,
+		identifier: 0x1234,
+		sequence:   0x0001,
 	}
+
+	checksum :=
+		0xffff - (binary.BigEndian.Uint16([]byte{icmp.typ, icmp.code}) +
+			icmp.identifier +
+			icmp.sequence)
+
+	icmp.checksum = checksum
+
+	return icmp
 }
 
 func (i *icmp) toBytes() []byte {
@@ -42,6 +46,14 @@ func (i *icmp) toBytes() []byte {
 
 	b := make([]byte, 2)
 	binary.BigEndian.PutUint16(b, i.checksum)
+	buf.Write(b)
+
+	b = make([]byte, 2)
+	binary.BigEndian.PutUint16(b, i.identifier)
+	buf.Write(b)
+
+	b = make([]byte, 2)
+	binary.BigEndian.PutUint16(b, i.sequence)
 	buf.Write(b)
 
 	buf.Write(i.data)

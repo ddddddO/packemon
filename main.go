088ad2f7e29988f8647d9714e4993c27b9ad2d95
ddddddO <lbfdeatq@gmail.com>
2024-05-04 09:37:48 +0900

    up

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/binary"
 	"flag"
 	"fmt"
@@ -121,49 +120,49 @@ func run(wantSend bool) error {
 		// return send(ethernetFrame, sock, addr)
 
 		// TCP
-		tcp := newTCPSyn()
-		ipv4 := newIPv4(IPv4_PROTO_TCP)
-		// https://atmarkit.itmedia.co.jp/ait/articles/0401/29/news080_2.html
-		// 「「チェックサム」フィールド：16bit幅」
-		tcp.checksum = func() uint16 {
-			pseudoTCPHeader := func() []byte {
-				var buf bytes.Buffer
-				b := make([]byte, 4)
-				binary.BigEndian.PutUint32(b, ipv4.srcAddr)
-				buf.Write(b)
+		// tcp := newTCPSyn()
+		// ipv4 := newIPv4(IPv4_PROTO_TCP)
+		// // https://atmarkit.itmedia.co.jp/ait/articles/0401/29/news080_2.html
+		// // 「「チェックサム」フィールド：16bit幅」
+		// tcp.checksum = func() uint16 {
+		// 	pseudoTCPHeader := func() []byte {
+		// 		var buf bytes.Buffer
+		// 		b := make([]byte, 4)
+		// 		binary.BigEndian.PutUint32(b, ipv4.srcAddr)
+		// 		buf.Write(b)
 
-				b = make([]byte, 4)
-				binary.BigEndian.PutUint32(b, ipv4.dstAddr)
-				buf.Write(b)
+		// 		b = make([]byte, 4)
+		// 		binary.BigEndian.PutUint32(b, ipv4.dstAddr)
+		// 		buf.Write(b)
 
-				padding := byte(0x00)
-				buf.WriteByte(padding)
+		// 		padding := byte(0x00)
+		// 		buf.WriteByte(padding)
 
-				buf.WriteByte(ipv4.protocol)
+		// 		buf.WriteByte(ipv4.protocol)
 
-				b = make([]byte, 2)
-				binary.BigEndian.PutUint16(b, uint16(len(tcp.toBytes())))
-				buf.Write(b)
+		// 		b = make([]byte, 2)
+		// 		binary.BigEndian.PutUint16(b, uint16(len(tcp.toBytes())))
+		// 		buf.Write(b)
 
-				return buf.Bytes()
-			}()
+		// 		return buf.Bytes()
+		// 	}()
 
-			var forTCPChecksum bytes.Buffer
-			forTCPChecksum.Write(pseudoTCPHeader)
-			forTCPChecksum.Write(tcp.toBytes())
-			return binary.BigEndian.Uint16(tcp.checkSum(forTCPChecksum.Bytes()))
-		}()
+		// 	var forTCPChecksum bytes.Buffer
+		// 	forTCPChecksum.Write(pseudoTCPHeader)
+		// 	forTCPChecksum.Write(tcp.toBytes())
+		// 	return binary.BigEndian.Uint16(tcp.checkSum(forTCPChecksum.Bytes()))
+		// }()
 
-		ipv4.data = tcp.toBytes()
-		ipv4.calculateTotalLength()
-		ipv4.calculateChecksum()
+		// ipv4.data = tcp.toBytes()
+		// ipv4.calculateTotalLength()
+		// ipv4.calculateChecksum()
 
-		dst := hardwareAddr([6]byte{0x00, 0x15, 0x5d, 0xb6, 0x16, 0xd6})
-		src := hardwareAddr(intf.HardwareAddr)
-		ethernetFrame := newEthernetFrame(dst, src, ETHER_TYPE_IPv4, ipv4.toBytes())
-		return send(ethernetFrame, sock, addr)
+		// dst := hardwareAddr([6]byte{0x00, 0x15, 0x5d, 0xb6, 0x16, 0xd6})
+		// src := hardwareAddr(intf.HardwareAddr)
+		// ethernetFrame := newEthernetFrame(dst, src, ETHER_TYPE_IPv4, ipv4.toBytes())
+		// return send(ethernetFrame, sock, addr)
 
-		// return form(sendForForm(sock, addr)) // Form のアクションで 送信した方が良さそうなのでこの形
+		return form(sendForForm(sock, addr)) // Form のアクションで 送信した方が良さそうなのでこの形
 	} else {
 		globalTviewGrid = tview.NewGrid()
 		globalTviewGrid.Box = tview.NewBox().SetBorder(true).SetTitle(" Packemon ")

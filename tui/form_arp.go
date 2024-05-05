da8ab2174c9347b5718a69838e414c64aef3865d
ddddddO <lbfdeatq@gmail.com>
2024-05-05 17:18:19 +0900

    split form

---
+++ tui/form_arp.go
@@ -0,0 +1,168 @@
+package tui
+
+import (
+	"encoding/binary"
+	"strings"
+
+	"github.com/ddddddO/packemon"
+	"github.com/rivo/tview"
+)
+
+func (t *tui) arpForm(sendFn func(*packemon.EthernetFrame) error, ethernetHeader *packemon.EthernetHeader, arp *packemon.ARP) *tview.Form {
+	arpForm := tview.NewForm().
+		AddTextView("ARP", "This section generates the ARP.\nIt is still under development.", 60, 4, true, false).
+		AddInputField("Hardware Type(hex)", DEFAULT_ARP_HARDWARE_TYPE, 6, func(textToCheck string, lastChar rune) bool {
+			if len(textToCheck) < 6 {
+				return true
+			} else if len(textToCheck) > 6 {
+				return false
+			}
+
+			b, err := strHexToBytes2(textToCheck)
+			if err != nil {
+				return false
+			}
+			arp.HardwareType = [2]byte(b)
+
+			return true
+		}, nil).
+		AddInputField("Protocol Type(hex)", DEFAULT_ARP_PROTOCOL_TYPE, 6, func(textToCheck string, lastChar rune) bool {
+			if len(textToCheck) < 6 {
+				return true
+			} else if len(textToCheck) > 6 {
+				return false
+			}
+
+			b, err := strHexToBytes2(textToCheck)
+			if err != nil {
+				return false
+			}
+			arp.ProtocolType = binary.BigEndian.Uint16(b)
+
+			return true
+		}, nil).
+		AddInputField("Hardware Size(hex)", DEFAULT_ARP_HARDWARE_SIZE, 4, func(textToCheck string, lastChar rune) bool {
+			if len(textToCheck) < 4 {
+				return true
+			} else if len(textToCheck) > 4 {
+				return false
+			}
+
+			b, err := strHexToUint8(textToCheck)
+			if err != nil {
+				return false
+			}
+			arp.HardwareAddrLength = b
+
+			return true
+		}, nil).
+		AddInputField("Protocol Size(hex)", DEFAULT_ARP_PROTOCOL_SIZE, 4, func(textToCheck string, lastChar rune) bool {
+			if len(textToCheck) < 4 {
+				return true
+			} else if len(textToCheck) > 4 {
+				return false
+			}
+
+			b, err := strHexToUint8(textToCheck)
+			if err != nil {
+				return false
+			}
+			arp.ProtocolLength = b
+
+			return true
+		}, nil).
+		AddInputField("Operation Code(hex)", DEFAULT_ARP_OPERATION, 6, func(textToCheck string, lastChar rune) bool {
+			if len(textToCheck) < 6 {
+				return true
+			} else if len(textToCheck) > 6 {
+				return false
+			}
+
+			b, err := strHexToBytes2(textToCheck)
+			if err != nil {
+				return false
+			}
+			arp.Operation = [2]byte(b)
+
+			return true
+		}, nil).
+		AddInputField("Sender Mac Addr(hex)", DEFAULT_ARP_SENDER_MAC, 14, func(textToCheck string, lastChar rune) bool {
+			if len(textToCheck) < 14 {
+				return true
+			} else if len(textToCheck) > 14 {
+				return false
+			}
+
+			b, err := strHexToBytes(textToCheck)
+			if err != nil {
+				return false
+			}
+			arp.SenderHardwareAddr = packemon.HardwareAddr(b)
+
+			return true
+		}, nil).
+		AddInputField("Sender IP Addr", DEFAULT_ARP_SENDER_IP, 15, func(textToCheck string, lastChar rune) bool {
+			count := strings.Count(textToCheck, ".")
+			if count < 3 {
+				return true
+			} else if count == 3 {
+				ip, err := strIPToBytes(textToCheck)
+				if err != nil {
+					return false
+				}
+
+				arp.SenderIPAddr = [4]byte(ip)
+				return true
+			}
+
+			return false
+		}, nil).
+		AddInputField("Target Mac Addr(hex)", DEFAULT_ARP_TARGET_MAC, 14, func(textToCheck string, lastChar rune) bool {
+			if len(textToCheck) < 14 {
+				return true
+			} else if len(textToCheck) > 14 {
+				return false
+			}
+
+			b, err := strHexToBytes(textToCheck)
+			if err != nil {
+				return false
+			}
+			arp.TargetHardwareAddr = packemon.HardwareAddr(b)
+
+			return true
+		}, nil).
+		AddInputField("Target IP Addr", DEFAULT_ARP_TARGET_IP, 15, func(textToCheck string, lastChar rune) bool {
+			count := strings.Count(textToCheck, ".")
+			if count < 3 {
+				return true
+			} else if count == 3 {
+				ip, err := strIPToBytes(textToCheck)
+				if err != nil {
+					return false
+				}
+
+				arp.TargetIPAddr = [4]byte(ip)
+				return true
+			}
+
+			return false
+		}, nil).
+		AddButton("Send!", func() {
+			ethernetFrame := &packemon.EthernetFrame{
+				Header: ethernetHeader,
+				Data:   arp.Bytes(),
+			}
+			if err := sendFn(ethernetFrame); err != nil {
+				t.app.Stop()
+			}
+		}).
+		AddButton("Prev", func() {
+			t.pages.SwitchToPage("Ethernet")
+		}).
+		AddButton("Quit", func() {
+			t.app.Stop()
+		})
+
+	return arpForm
+}
---
+++ tui/form_icmp.go
@@ -0,0 +1,110 @@
+package tui
+
+import (
+	"encoding/binary"
+	"time"
+
+	"github.com/ddddddO/packemon"
+	"github.com/rivo/tview"
+)
+
+func (t *tui) icmpForm(sendFn func(*packemon.EthernetFrame) error, ethernetHeader *packemon.EthernetHeader, ipv4 *packemon.IPv4, icmp *packemon.ICMP) *tview.Form {
+	icmpForm := tview.NewForm().
+		AddTextView("ICMP", "This section generates the ICMP.\nIt is still under development.", 60, 4, true, false).
+		AddInputField("Type(hex)", DEFAULT_ICMP_TYPE, 4, func(textToCheck string, lastChar rune) bool {
+			if len(textToCheck) < 4 {
+				return true
+			} else if len(textToCheck) > 4 {
+				return false
+			}
+
+			b, err := strHexToUint8(textToCheck)
+			if err != nil {
+				return false
+			}
+			icmp.Typ = uint8(b)
+
+			return true
+		}, nil).
+		AddInputField("Code(hex)", DEFAULT_ICMP_CODE, 4, func(textToCheck string, lastChar rune) bool {
+			if len(textToCheck) < 4 {
+				return true
+			} else if len(textToCheck) > 4 {
+				return false
+			}
+
+			b, err := strHexToUint8(textToCheck)
+			if err != nil {
+				return false
+			}
+			icmp.Code = uint8(b)
+
+			return true
+		}, nil).
+		AddInputField("Identifier(hex)", DEFAULT_ICMP_IDENTIFIER, 6, func(textToCheck string, lastChar rune) bool {
+			if len(textToCheck) < 6 {
+				return true
+			} else if len(textToCheck) > 6 {
+				return false
+			}
+
+			b, err := strHexToBytes2(textToCheck)
+			if err != nil {
+				return false
+			}
+			icmp.Identifier = binary.BigEndian.Uint16(b)
+
+			return true
+		}, nil).
+		AddInputField("Sequence(hex)", DEFAULT_ICMP_SEQUENCE, 6, func(textToCheck string, lastChar rune) bool {
+			if len(textToCheck) < 6 {
+				return true
+			} else if len(textToCheck) > 6 {
+				return false
+			}
+
+			b, err := strHexToBytes2(textToCheck)
+			if err != nil {
+				return false
+			}
+			icmp.Sequence = binary.BigEndian.Uint16(b)
+
+			return true
+		}, nil).
+		AddButton("Send!", func() {
+			// TODO: timestamp関数化
+			icmp.Data = func() []byte {
+				now := time.Now().Unix()
+				b := make([]byte, 4)
+				binary.LittleEndian.PutUint32(b, uint32(now))
+				return binary.LittleEndian.AppendUint32(b, 0x00000000)
+			}()
+			// 前回Send分が残ってると計算誤るため
+			icmp.Checksum = 0x0
+			icmp.Checksum = func() uint16 {
+				b := make([]byte, 2)
+				binary.LittleEndian.PutUint16(b, icmp.CalculateChecksum())
+				return binary.BigEndian.Uint16(b)
+			}()
+			ipv4.Data = icmp.Bytes()
+			ipv4.CalculateTotalLength()
+			// 前回Send分が残ってると計算誤るため
+			ipv4.HeaderChecksum = 0x0
+			ipv4.CalculateChecksum()
+			ethernetFrame := &packemon.EthernetFrame{
+				Header: ethernetHeader,
+				Data:   ipv4.Bytes(),
+			}
+			if err := sendFn(ethernetFrame); err != nil {
+				t.app.Stop()
+			}
+		}).
+		AddButton("Prev", func() {
+			t.pages.SwitchToPage("IPv4")
+		}).
+		AddButton("Quit", func() {
+			t.app.Stop()
+		})
+
+	return icmpForm
+}

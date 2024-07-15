5d86a970c3a887c91385fd7f29c7f5d93810e6b6
ddddddO <lbfdeatq@gmail.com>
2024-07-15 20:26:49 +0900

    wip client hello

--- internal/debugging/networkinterface.go
+++ internal/debugging/networkinterface.go
@@ -285,7 +285,7 @@ func (dnw *debugNetworkInterface) SendTCP3wayhandshake(firsthopMACAddr [6]byte) 
 }
 
 func (dnw *debugNetworkInterface) SendTCP3wayAndTLShandshake(firsthopMACAddr [6]byte) error {
-	var srcPort uint16 = 0xa022
+	var srcPort uint16 = 0xa026
 	var dstPort uint16 = 0x01BB       // 443
 	var srcIPAddr uint32 = 0xac184fcf // 172.23.242.78
 	var dstIPAddr uint32 = 0xac4348dc // zenn / 172.67.72.220
--- tls.go
+++ tls.go
@@ -18,10 +18,14 @@ func NewTLSClientHello() *TLSClientHello {
 
 	return &TLSClientHello{
 		// 03 03 = TLS1.2
-		RecordHeader:    []byte{0x16, 0x03, 0x03, 0x00, 0xa5},
-		HandshakeHeader: []byte{CLIENT_HELLO, 0x00, 0x00, 0xa1},
-		ClientVersion:   []byte{0x03, 0x03},
-		// TODO: 以降
+		RecordHeader:      []byte{0x16, 0x03, 0x03, 0x00, 0x4d}, // 4d = 77byte len
+		HandshakeHeader:   []byte{CLIENT_HELLO, 0x00, 0x00, 0xa1},
+		ClientVersion:     []byte{0x03, 0x03},
+		ClientRandom:      make([]byte, 32),
+		SessionID:         []byte{0x00},
+		CipherSuites:      []byte{0x00, 0x20, 0xcc, 0xa8, 0xcc, 0xa9, 0xc0, 0x2f, 0xc0, 0x30, 0xc0, 0x2b, 0xc0, 0x2c, 0xc0, 0x13, 0xc0, 0x09, 0xc0, 0x14, 0xc0, 0x0a, 0x00, 0x9c, 0x00, 0x9d, 0x00, 0x2f, 0x00, 0x35, 0xc0, 0x12, 0x00, 0x0a},
+		CompressionMethod: []byte{0x01, 0x00},
+		ExtensionsLength:  []byte{0x00, 0x00},
 	}
 }
 
@@ -30,5 +34,11 @@ func (tch *TLSClientHello) Bytes() []byte {
 	buf = append(buf, tch.RecordHeader...)
 	buf = append(buf, tch.HandshakeHeader...)
 	buf = append(buf, tch.ClientVersion...)
+	buf = append(buf, tch.ClientRandom...)
+	buf = append(buf, tch.SessionID...)
+	buf = append(buf, tch.CipherSuites...)
+	buf = append(buf, tch.CompressionMethod...)
+	buf = append(buf, tch.ExtensionsLength...)
+	buf = append(buf, tch.Extentions...)
 	return buf
 }

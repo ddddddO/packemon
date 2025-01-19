72269b45594a1c69082ad34722aba087d15c16a5
ddddddO <lbfdeatq@gmail.com>
2025-01-19 16:48:45 +0900

    view for ipv6

--- networkinterface.go
+++ networkinterface.go
@@ -90,7 +90,6 @@ func hton(i uint16) uint16 {
 }
 
 func (nw *NetworkInterface) Send(ethernetFrame *EthernetFrame) error {
-	// return unix.Sendto(nw.Socket, ethernetFrame.Bytes(), 0, &nw.SocketAddr)
 	return unix.Sendto(nw.Socket, ethernetFrame.Bytes(), 0, &nw.SocketAddr)
 }
 
@@ -214,11 +213,51 @@ func ParsedPacket(recieved []byte) *Passive {
 		}
 	case ETHER_TYPE_IPv6:
 		ipv6 := ParsedIPv6(ethernetFrame.Data)
-		return &Passive{
-			EthernetFrame: ethernetFrame,
-			IPv6:          ipv6,
-		}
 
+		switch ipv6.NextHeader {
+		case IPv6_NEXT_HEADER_ICMPv6:
+			// TODO:
+			return &Passive{
+				EthernetFrame: ethernetFrame,
+				IPv6:          ipv6,
+			}
+		case IPv6_NEXT_HEADER_UDP:
+			udp := ParsedUDP(ipv6.Data)
+			passive := &Passive{
+				EthernetFrame: ethernetFrame,
+				IPv6:          ipv6,
+				UDP:           udp,
+			}
+
+			// DNS以外は一旦udpまでのみviewする
+			if udp.DstPort != PORT_DNS && udp.SrcPort != PORT_DNS {
+				return passive
+			}
+
+			// TODO: 53確かtcpもあったからそれのハンドリング考慮するいつか
+			// TODO: nslookup github.com でipv6用のDNSクエリ・レスポンスも返ってきてるのでそれも対応
+			//       query.type == AAAA で判別可能
+			flags := binary.BigEndian.Uint16(udp.Data[2:4])
+			if udp.DstPort == PORT_DNS && IsDNSRequest(flags) {
+				dns := ParsedDNSRequest(udp.Data)
+				passive.DNS = dns
+				return passive
+			}
+
+			if udp.SrcPort == PORT_DNS && IsDNSResponse(flags) {
+				dns := ParsedDNSResponse(udp.Data)
+				passive.DNS = dns
+				return passive
+			}
+
+			return passive
+
+		default:
+			return &Passive{
+				EthernetFrame: ethernetFrame,
+				IPv6:          ipv6,
+			}
+		}
 	default:
 		return &Passive{
 			EthernetFrame: ethernetFrame,

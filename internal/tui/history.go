0a2d76f64786ff86e08b48770610743fcfe5a7e4
ddddddO <lbfdeatq@gmail.com>
2025-03-15 18:38:33 +0900

    rm title

--- internal/tui/history.go
+++ internal/tui/history.go
@@ -13,7 +13,6 @@ import (
 
 func NewPacketsHistoryTable() *tview.Table {
 	h := tview.NewTable()
-	h.SetTitle(TITLE_MONITOR)
 	h.SetTitleAlign(tview.AlignLeft)
 	h.SetBorder(true)
 	h.ScrollToBeginning()

4815f9a3b7d65224a97cd5900866158649c53fad
ddddddO <lbfdeatq@gmail.com>
2025-09-08 19:28:01 +0900

    add caution for TLS on Generator

--- internal/tui/generator/form_tlsv1_2.go
+++ internal/tui/generator/form_tlsv1_2.go
@@ -6,6 +6,7 @@ import (
 
 func (g *generator) tlsv1_2Form() *tview.Form {
 	tlsv1_2Form := tview.NewForm().
+		AddTextView("!!caution!!", "Experimental implementation and may not work properly.", 60, 2, true, false).
 		AddTextView("TLSv1.2", "TLS v1.2 has been selected;\nafter TLS v1.2 handshake,\nthe request is made with upper layer encrypted.", 60, 4, true, false).
 		AddButton("Quit", func() {
 			g.app.Stop()
--- internal/tui/generator/form_tlsv1_3.go
+++ internal/tui/generator/form_tlsv1_3.go
@@ -6,6 +6,7 @@ import (
 
 func (g *generator) tlsv1_3Form() *tview.Form {
 	tlsv1_3Form := tview.NewForm().
+		AddTextView("!!caution!!", "Experimental implementation and may not work properly.", 60, 2, true, false).
 		AddTextView("TLSv1.3", "TLS v1.3 has been selected;\nafter TLS v1.3 handshake,\nthe request is made with upper layer encrypted.", 60, 4, true, false).
 		AddButton("Quit", func() {
 			g.app.Stop()

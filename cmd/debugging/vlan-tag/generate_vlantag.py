179391ca38ecbd1da750884811d9c40e1d56c511
ddddddO <lbfdeatq@gmail.com>
2026-09-19 19:15:59 +0900

    fix

--- cmd/debugging/vlan-tag/generate_vlantag.py
+++ cmd/debugging/vlan-tag/generate_vlantag.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from scapy.all import Ether, Dot1Q, IP, ICMPv4, sendp
+from scapy.all import Ether, Dot1Q, IP, ICMP, sendp
 
 # 1. L2 (Ethernetヘッダ)
 # dst: 宛先MACアドレス, src: 送信元MACアドレス
@@ -14,8 +14,8 @@ vlan_layer = Dot1Q(vlan=10)
 # src: 送信元IP, dst: 宛先IP
 ip_layer = IP(src="192.168.10.100", dst="192.168.10.1")
 
-# 4. L4 (ICMPv4ヘッダ)
-icmp_layer = ICMPv4()
+# 4. L4 (ICMPヘッダ)
+icmp_layer = ICMP()
 
 # フレームを結合: L2 / VLAN / L3 / L4 の順
 packet = eth_layer / vlan_layer / ip_layer / icmp_layer
